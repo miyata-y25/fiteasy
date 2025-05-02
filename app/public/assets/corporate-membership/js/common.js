33c36c6aa1cd1fb1ce9361f5b46551a67545e5f9
miyatayuki <miyatayuki25@gmail.com>
2025-05-02 19:50:42 +0900

    最新

---
+++ app/public/assets/corporate-membership/js/common.js
@@ -0,0 +1,89 @@
+(function ($) {
+    //ページ内スクロール
+    $('a[href*="#"]').on('click',function() {
+        var target = $(this).attr('href');
+        var position = $(target).offset().top;
+        var $header     = $('.site-header');
+        var fheaderH    = $header.innerHeight();
+        $('body,html').animate({scrollTop:position - 0}, 600, 'swing');
+        return false;
+    });
+    //スマホグロナビボタン
+    $('.menu-btn').on('click',function(){
+        $(this).toggleClass('menu-btn--opened');
+        $('body').toggleClass('opened');
+        $('.header-cont').stop().fadeToggle();
+    });
+
+    $(window).on('load scroll', function(){
+        var animTrigger = $('.anim');
+        $(animTrigger).each(function(){
+            var scroll = $(window).scrollTop(),
+                elemTop = $(this).offset().top,
+                windowHeight = $(window).height();
+
+            if (scroll > elemTop - windowHeight + 200){
+                $(this).addClass('is-animated');
+            } else {
+            }
+        });
+    });
+    $(window).trigger('scroll');
+    $('.is-animated').removeClass('is-animated');
+})(jQuery);
+
+
+$(function($){
+    var $header     = $('.site-header');
+    var fheaderH    = $header.innerHeight();
+
+    scrollCheck();
+    $(window).scroll(scrollCheck);
+
+    function scrollCheck(){
+        var scrl = $(window).scrollTop();
+
+        if (scrl > 0 ) {
+            $header.addClass("cls");
+        } else {
+            $header.removeClass("cls");
+        }
+    }
+});
+
+$(function(){
+	$('.-tablist input').on('click', function(){
+		var index = $('.-tablist input').index(this);
+		$('.-tabflex').eq(index).addClass('-act').siblings('div').removeClass('-act');
+        $('body,html').animate({scrollTop:$('#-tablink').offset().top - $('.site-header').innerHeight()}, 400, 'swing');
+	});
+});
+
+$(document).ready(function () {
+    const itemHeights = [];
+    let returnHeight;
+  
+    $('.jsAccordionText').each(function () {
+      const thisHeight = $(this).height();
+      itemHeights.push(thisHeight);
+      $(this).addClass('isHide');
+      returnHeight = $(this).height();
+    });
+  
+    $('.jsAccordionBtn').click(function () {
+      if (!$(this).hasClass('isShow')) {
+        const index = $(this).index('.jsAccordionBtn');
+        const addHeight = itemHeights[index];
+        $(this)
+          .addClass('isShow')
+          .next()
+          .animate({ height: addHeight });
+      } else {
+        $(this)
+          .removeClass('isShow')
+          .next()
+          .animate({ height: returnHeight }, 300);
+      }
+    });
+  });
+
